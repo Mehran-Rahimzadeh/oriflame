735ec3adfd4369e3245f085f052887779148988b
Mehran-Rahimzadeh <mehran1988r@gmail.com>
2023-06-24 10:25:32 +0330

    making js function for hovering list of best hovers

--- js/main.js
+++ js/main.js
@@ -1,17 +1,40 @@
+
 // ----------iuser icon change---------
 document.querySelector("div.menuTop>div:nth-of-type(1)>span:nth-of-type(2)>a:nth-of-type(1)>i").
 addEventListener("mouseenter",function(){
     this.classList.remove("bi-person")
     this.classList.add("bi-person-fill")
-    
 })
 document.querySelector("div.menuTop>div:nth-of-type(1)>span:nth-of-type(2)>a:nth-of-type(1)>i").
 addEventListener("mouseleave",function(){
     this.classList.remove("bi-person-fill")
     this.classList.add("bi-person")
-    
 })
+//-----------End of  user icon change---------
 
-document.querySelector("div.menuTop>div:nth-of-type(1)>span:nth-of-type(1)>a>i")
-console.log(document.querySelector("div.menuTop>div:nth-of-type(1)>span:nth-of-type(1)>a>i"))
-//-----------End of  iuser icon change---------
+
+
+// -------Best Offers-------
+// --------hover-Function---------
+function hover(id_1,id_2){
+    document.querySelector(id_1).
+    addEventListener("mouseenter",function(){
+        document.getElementById(id_2).classList.remove("noneActive");
+        document.getElementById(id_2).classList.add("active")
+   })}
+function not_hover(id_1,id_2){
+    document.querySelector(id_1).
+    addEventListener("mouseleave",function(){
+        document.getElementById(id_2).classList.remove("active");
+        document.getElementById(id_2).classList.add("noneaAtive")
+    })}
+// --------End of hover-Function---------
+// ----calling hover---
+hover("#Inspiration","Inspiration1")
+not_hover("#Inspiration","Inspiration1")
+
+// ----End of calling hover---
+
+
+ 
+// -------End of Best Offers-------
