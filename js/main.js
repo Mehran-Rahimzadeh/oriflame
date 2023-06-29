9cf06a56360c95c0cbd180dfec9320b1dfc8ae06
Mehran-Rahimzadeh <mehran1988r@gmail.com>
2023-06-29 17:29:19 +0330

    adding subMenu with hover and dropdown feature

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // ----------iuser icon change---------
 document.querySelector("div.menuTop>div:nth-of-type(1)>span:nth-of-type(2)>a:nth-of-type(1)>i").
 addEventListener("mouseenter",function(){
@@ -14,9 +13,9 @@ addEventListener("mouseleave",function(){
 
 
 
-// -------Best Offers-------
-// --------hover-Function---------
 
+
+// --------hover-Function---------
 function hover(id_1,id_2){
     document.querySelector(id_1).
     addEventListener("mouseenter",function(){
@@ -24,28 +23,34 @@ function hover(id_1,id_2){
         document.getElementById(id_2).classList.add("active")
    })}
 function not_hover(id_1,id_2){
-   
     document.querySelector(id_1).
     addEventListener("mouseleave",function(){
         document.getElementById(id_2).classList.remove("active");
         document.getElementById(id_2).classList.add("noneaAtive")
     })}
 // --------End of hover-Function---------
-// ----calling hover---
-// hover("#eCatalogue","eCatalogue1")
-// not_hover("#eCatalogue","eCatalogue1");
 
-// hover("#Inspiration","Inspiration1")
-// not_hover("#Inspiration","Inspiration1")
 
-// // ----End of calling hover---
-// function upper(id_1,id_2){
-//     document.querySelector(id_1).
-//     addEventListener("mouseenter",function(){
-//         // document.getElementById(id_2).classList.remove("noneActive");
-//         document.getElementById(id_2).classList.add("upper")
-//    })}
-//    upper("#Inspiration","Inspiration1");
+
+
 
  
-// -------End of Best Offers-------
+// ---  ----List Icon VS Close icon-------
+let menuIcon =document.querySelector("div.menuTop>div:nth-of-type(1)>span>a:nth-of-type(1)>button>i:nth-of-type(1)");
+let closeIcon =document.querySelector("div.menuTop>div:nth-of-type(1)>span>a:nth-of-type(1)>button>i:nth-of-type(2)");
+console.log(menuIcon)
+console.log(closeIcon)
+
+menuIcon.addEventListener("click",function(){
+    this.classList.add("noneActive");
+    closeIcon.classList.add("active")
+},false)
+
+closeIcon.addEventListener("click",function(){
+    this.classList.remove("active");
+    menuIcon.classList.remove("noneActive")
+},false)
+
+
+
+// -------End of List Icon VS Close icon-------
